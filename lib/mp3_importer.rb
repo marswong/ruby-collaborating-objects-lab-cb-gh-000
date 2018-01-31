649f739fe16e53a4592b656f3f2be488481c38b2
Mars Wong <marswong618@gmail.com>
2018-01-31 02:13:55 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -5,7 +5,9 @@ class MP3Importer
   def initialize(path)
     if Dir.exist?(path)
       @path = path
-      Dir.entries(path).select { |filename| filename.end_with?(".mp3") }
+      Dir.entries(path).each do |filename|
+        self.files << filename if filename.end_with?(".mp3")        
+      end
     end
   end
 
