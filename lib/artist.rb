97e04861dc97e499604da4517bdebb46e22c85eb
Mars Wong <marswong618@gmail.com>
2018-01-31 01:59:47 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,10 +15,14 @@ class Artist
   end
 
   def save
-    self.all << self    
+    self.all << self
   end
 
   def find_or_create_by_name(name)
     self.all.index { |obj| obj.name == name } || self.class.new(name)
+  end
+
+  def print_songs
+    self.all.each { |k, v| puts v }
   end
 end
